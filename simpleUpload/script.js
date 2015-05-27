a5c199e3f45fd351e3699ac5da857ae3469ed6bf
lokal-profil <lokal.profil@gmail.com>
2015-05-27 13:51:19 +0200

    Handle empty filenames, linebreaks in code

--- simpleUpload/script.js
+++ simpleUpload/script.js
@@ -33,8 +33,8 @@ function processFilename() {
     // reset later fields
     $('#reflect').empty();
     $('#button').prop('disabled', true);
-    $('#submit_button').addClass('hidden');
     $('#thumbDiv').addClass('hidden');
+    $('#mapillary').removeClass('highlighted');
 
     // test filename
     var run = true;
@@ -48,13 +48,20 @@ function processFilename() {
         $('#mapillary').val(input);
     }
     // run if mapillary_id is likely to be valid
-    queryMapillary(input);
-    $('#button').prop('disabled', false);
+    if (run) {
+        queryMapillary(input);
+        $('#button').prop('disabled', false);
+    }
+    else {
+        $('#button').prop('disabled', false);
+    }
 }
 
 // query mapillary and process response
 function queryMapillary(file_key) {
-    var url = 'https://a.mapillary.com/v2/g/' + file_key + "?client_id=NzNRM2otQkR2SHJzaXJmNmdQWVQ0dzoxNjQ3MDY4ZTUxY2QzNGI2";
+    var url = 'https://a.mapillary.com/v2/g/' +
+              file_key +
+              "?client_id=NzNRM2otQkR2SHJzaXJmNmdQWVQ0dzoxNjQ3MDY4ZTUxY2QzNGI2";
 
     $.ajax({
         url: url,
@@ -80,15 +87,23 @@ function queryMapillary(file_key) {
                     '|lon=' + parseddata.nodes[0].lon +
                     '|ca=' + parseddata.nodes[0].ca +
                     '}}';
-                var destFile = parseddata.nodes[0].location + ' - Mapillary (' + parseddata.nodes[0].key + ').jpg';
-                var imageurl = parseddata.nodes[0].image.replace('thumb-1024.jpg', 'thumb-2048.jpg');  //request larger size
-                var magnusurl = '//tools.wmflabs.org/url2commons/index.html?urls=' + imageurl + ' ' + destFile + '|' + encodeURIComponent(uploadDescription) + '&desc=$DESCRIPTOR$';
+                var destFile = parseddata.nodes[0].location +
+                               ' - Mapillary (' +
+                               parseddata.nodes[0].key +
+                               ').jpg';
+                //request larger size
+                var imageurl = parseddata.nodes[0].image.replace('thumb-1024.jpg', 'thumb-2048.jpg');
+                var magnusurl = '//tools.wmflabs.org/url2commons/index.html?' +
+                                //'run=1&' +
+                                'urls=' + imageurl + ' ' +
+                                destFile + '|' +
+                                encodeURIComponent(uploadDescription) +
+                                '&desc=$DESCRIPTOR$';
                 // Ready to produce upload link
                 $('#thumb').attr("src", imageurl);
-                $('#thumbDiv').removeClass('hidden');
                 $('#submit_button').attr("href", magnusurl);
                 $('#submit_button').html('<big>Upload as</big><br/>' + destFile);
-                $('#submit_button').removeClass('hidden');
+                $('#thumbDiv').removeClass('hidden');
             }
         },
         error: function (jqxhr, textStatus, errorThrown) {
